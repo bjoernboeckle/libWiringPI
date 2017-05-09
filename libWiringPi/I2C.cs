7828bc270b38f921a0f033de3dd47480ea2b7551
Björn Böckle <bjoern@boeckle-net.de>
2017-05-09 11:06:04 +0200

    Changed locally

--- libWiringPi/I2C.cs
+++ libWiringPi/I2C.cs
@@ -4,7 +4,7 @@ namespace libWiringPi
 {
     public class I2C
     {
-        // Did a very similar change in master
+        // Removed locally
         //Changes done to new branch
 
         const string I2C_LIBRARY = "libwiringPiI2C.so";
