b544a29eca50415f490decb2c8cd426b3a92fce2
Björn Böckle <bjoern@boeckle-net.de>
2017-05-09 13:57:38 +0200

    Change in feature branch to test pull request

--- libWiringPi/I2C.cs
+++ libWiringPi/I2C.cs
@@ -4,10 +4,7 @@ namespace libWiringPi
 {
     public class I2C
     {
-        // Changed remote should have a conflict
-        // Changed remote
-        // Removed locally
-        // Changes done to new branch
+        // This time really working on the feature branch
 
         const string I2C_LIBRARY = "libwiringPiI2C.so";
 
